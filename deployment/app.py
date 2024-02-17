2a2b267d4268e051110ccaafc2fd28856e006e1d
alex <wujian3659@163.com>
2024-02-17 22:33:55 +0800

    remove unused

--- deployment/app.py
+++ deployment/app.py
@@ -17,31 +17,13 @@ AWS_ENV = cdk.Environment(account=ACCOUNT, region=REGION)
 env = AWS_ENV
 print(env)
 app = cdk.App()
-if app.node.try_get_context('vpc_deployment'):
-    vpcstack = VpcStack(app, 'VpcStack',env=env, description="Guidance for Custom Search of an Enterprise Knowledge Base on AWS - (SO9251)")
-    searchstack = OpenSearchVPCStack(app, "OpenSearchVPCStack",vpc=vpcstack.vpc, env=env, description="Guidance for Custom Search of an Enterprise Knowledge Base on AWS - (SO9251)")
-    searchstack.add_dependency(vpcstack)
-    search_engine_key = searchstack.search_domain_endpoint
-    lambdastack = LambdaVPCStack(app, "LambdaVPCStack", search_engine_key,vpc=vpcstack.vpc, env=env, description="Guidance for Custom Search of an Enterprise Knowledge Base on AWS - (SO9251)")
-    lambdastack.add_dependency(searchstack)
-    bedrockstack = BedrockStack( app, "BedrockStack", env=env)
-else:
-    if app.node.try_get_context('search_engine_opensearch'):
-        searchstack = OpenSearchStack(app, "OpenSearchStack", env=env, description="Guidance for Custom Search of an Enterprise Knowledge Base on AWS - (SO9251)")
-        search_engine_key = searchstack.search_domain_endpoint
-    else:
-        searchstack = KendraStack(app, "KendraStack", env=env, description="Guidance for Custom Search of an Enterprise Knowledge Base on AWS - (SO9251)")
-        search_engine_key = searchstack.kendra_index_id        
-    lambdastack = LambdaStack(app, "LambdaStack", search_engine_key=search_engine_key, env=env, description="Guidance for Custom Search of an Enterprise Knowledge Base on AWS - (SO9251)")
-    lambdastack.add_dependency(searchstack)
-    bedrockstack = BedrockStack( app, "BedrockStack", env=env)
+
+searchstack = OpenSearchStack(app, "OpenSearchStack", env=env, description="Guidance for Custom Search of an Enterprise Knowledge Base on AWS - (SO9251)")
+search_engine_key = searchstack.search_domain_endpoint
+lambdastack = LambdaStack(app, "LambdaStack", search_engine_key=search_engine_key, env=env, description="Guidance for Custom Search of an Enterprise Knowledge Base on AWS - (SO9251)")
+lambdastack.add_dependency(searchstack)
+bedrockstack = BedrockStack( app, "BedrockStack", env=env)
 notebookstack = NotebookStack(app, "NotebookStack", search_engine_key=search_engine_key, env=env, description="Guidance for Custom Search of an Enterprise Knowledge Base on AWS - (SO9251)")
 notebookstack.add_dependency(searchstack)
-
-  
-if('bot' in app.node.try_get_context("extension")):   
-    botstack = BotStack(app, "BotStack", env=env, description="Guidance for Custom Search of an Enterprise Knowledge Base on AWS - (SO9251)")
-    botstack.add_dependency(lambdastack)
-
 
 app.synth()
--- deployment/lib/ss_osstack.py
+++ deployment/lib/ss_osstack.py
@@ -30,23 +30,9 @@ class OpenSearchStack(Stack):
       secret_name = "opensearch-master-user"
     )
 
-    #XXX: aws cdk elastsearch example - https://github.com/aws/aws-cdk/issues/2873
-    # You should camelCase the property names instead of PascalCase
     ops_domain = aws_opensearchservice.Domain(self, "OpenSearch",
       domain_name=ops_domain_name,
-      #XXX: Supported versions of OpenSearch and Elasticsearch
-      # https://docs.aws.amazon.com/opensearch-service/latest/developerguide/what-is.html#choosing-version
       version=aws_opensearchservice.EngineVersion.OPENSEARCH_1_3,
-      #XXX: Amazon OpenSearch Service - Current generation instance types
-      # https://docs.aws.amazon.com/opensearch-service/latest/developerguide/supported-instance-types.html#latest-gen
-      # access_policies=[_iam.PolicyStatement(
-      #           actions=[
-      #             "es:Describe*",
-      #             "es:List*",
-      #             "es:Get*",
-      #             "es:ESHttp*"], 
-      #           resources=['*'] # 可根据需求进行更改
-      #       ),],
       capacity={
         "master_nodes": 0,
         "master_node_instance_type": "m5.xlarge.search",
@@ -57,36 +43,20 @@ class OpenSearchStack(Stack):
         "volume_size": 20,
         "volume_type": aws_ec2.EbsDeviceVolumeType.GP3
       },
-      #XXX: az_count must be equal to vpc subnets count.
-      # zone_awareness={
-      #   "availability_zone_count": 2,
-      #   "enabled": True
-      # },
       fine_grained_access_control=aws_opensearchservice.AdvancedSecurityOptions(
         master_user_name=master_user_secret.secret_value_from_json("username").unsafe_unwrap(),
         master_user_password=master_user_secret.secret_value_from_json("password")
       ),
-      # Enforce HTTPS is required when fine-grained access control is enabled.
       enforce_https=True,
-      # Node-to-node encryption is required when fine-grained access control is enabled
       node_to_node_encryption=True,
-      # Encryption-at-rest is required when fine-grained access control is enabled.
       encryption_at_rest={
         "enabled": True
       },
       use_unsigned_basic_auth=True,   #default: False
-      # security_groups=[sg_opensearch_cluster],
-      #XXX: For domains running OpenSearch or Elasticsearch 5.3 and later, OpenSearch Service takes hourly automated snapshots
-      # Only applies for Elasticsearch versions below 5.3
-      # automated_snapshot_start_hour=17, # 2 AM (GTM+9)
-      # vpc=vpc,
-      #XXX: az_count must be equal to vpc subnets count.
-      # vpc_subnets=[aws_ec2.SubnetSelection(one_per_az=True, subnet_type=aws_ec2.SubnetType.PRIVATE_WITH_EGRESS)],
       removal_policy=cdk.RemovalPolicy.DESTROY # default: cdk.RemovalPolicy.RETAIN
     )
     cdk.Tags.of(ops_domain).add('Name', 'smartsearch-ops')
 
-    # self.sg_search_client = sg_use_opensearch
     self.search_domain_endpoint = ops_domain.domain_endpoint
     self.search_domain_arn = ops_domain.domain_arn
 
